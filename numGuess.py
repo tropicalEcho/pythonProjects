ab5489e825b4581f95e3fbbd053bad9559f5a7d1
R4JV12 <rajvir0650@gmail.com>
2024-12-23 13:53:46 +0530

    Update numGuess.py

--- numGuess.py
+++ numGuess.py
@@ -1,20 +1,34 @@
+import sys, os
 from random import randint
 
-try: number = randint(int(input("Initial: ")), int(input("Final: ")))
-except ValueError: print("Error!"); exit()
+try: 
+    number = randint(int(input("Initial: ")), int(input("Final: ")))
+except ValueError: print("Error!")
+    sys.exit("Goodbye!")
 
 while True:
     try: 
         total_guesses = int(input("How many chances? [1, 25]: "))
-        if 1 <= total_guesses <= 25: break
-    except ValueError: print("Error!")
+        if 1 <= total_guesses <= 25: 
+            break
+    except ValueError: 
+        print("Error!")
 
 for guesses in range(1, total_guesses + 1):
-    try: guess = int(input(f"Guess {guesses}: "))
-    except: print("Error!"); exit()
-    if guess == 0: print(f"Number: {number}"); break
-    elif guess == number: print("You Won!"); break
-    elif guess > number: print("Try something less than that")
-    else: print("Try with something more than that")
+    try: 
+        guess = int(input(f"Guess {guesses}: "))
+    except: 
+        print("Error!")
+        sys.exit("Goodbye!)
+    if guess == 0: 
+        print(f"Number: {number}")
+        break
+    elif guess == number: 
+        print("You Won!")
+        break
+    elif guess > number: 
+        print("Try something less than that")
+    else: 
+        print("Try with something more than that")
 
 else: print("You Lost!")
